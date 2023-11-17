fd763077aeffad7e6e49820e0c55212aca46e954
songchongyang.scy <songchongyang.scy@alibaba-inc.com>
2023-11-17 15:50:57 +0800

    404. 左叶子之和

--- src/tree/SumOfLeft.java
+++ src/tree/SumOfLeft.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import tree.common.TreeNode;
 
 /**
@@ -11,24 +14,56 @@ public class SumOfLeft {
     /**
      * 方法一：递归
      */
+    //public int sumOfLeftLeaves(TreeNode root) {
+    //    if (root == null) {
+    //        return 0;
+    //    }
+    //
+    //    TreeNode leftNode = root.left;
+    //    // 没有左子节点
+    //    if (leftNode == null) {
+    //        return sumOfLeftLeaves(root.right);
+    //    }
+    //    // 左子节点是叶子节点
+    //    else if (isLeafNode(leftNode)) {
+    //        return leftNode.val + sumOfLeftLeaves(root.right);
+    //    }
+    //    // 左子节点不是叶子节点
+    //    else {
+    //        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    //    }
+    //}
+
+
+    /**
+     * 方法二：迭代
+     */
     public int sumOfLeftLeaves(TreeNode root) {
         if (root == null) {
             return 0;
         }
 
-        TreeNode leftNode = root.left;
-        // 没有左子节点
-        if (leftNode == null) {
-            return sumOfLeftLeaves(root.right);
+        int result = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null && isLeafNode(node.left)) {
+                result += node.left.val;
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
         }
-        // 左子节点是叶子节点
-        else if (leftNode.left == null && leftNode.right == null) {
-            return leftNode.val + sumOfLeftLeaves(root.right);
-        }
-        // 左子节点不是叶子节点
-        else {
-            return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
-        }
+
+        return result;
+    }
+
+    private boolean isLeafNode(TreeNode left) {
+        return left.left == null && left.right == null;
     }
 
 }
