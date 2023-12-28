5a524ce704aa26281990182c7204ce5cffd51a00
songchongyang.scy <songchongyang.scy@alibaba-inc.com>
2023-12-28 11:29:09 +0800

    手撕快速排序 refact

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -9,43 +9,44 @@ public class QuickSort {
 
     /**
      * [left ~ right] 为当前需要排序的区间
-     * 每次在当前区间中选出最中间位置的元素 mid 作为基准，然后把比 arr[mid] 大的元素全部移动到 mid 左边，把比 arr[mid] 小的元素全部移动到 mid 右边
-     * 然后递归地排序 mid 左边的部分和 mid 右边的部分
+     * 每次在当前区间中选出一个元素 mid 作为基准，然后把比 mid 大的元素全部移动到比 mid 小的元素的右边
+     * 然后递归地排序左边比 mid 大的元素 和 右边比 mid 小的元素
      *
      * 具体做法：
-     *    用两个指针，一个从 left 到 mid 遍历，一个从 right 到 mid 遍历
-     *    当 left 遍历到一个比 arr[mid] 大的元素就停下，当 right 遍历到一个比 arr[mid] 小的元素就停下，然后交换 arr[left] 和 arr[right]
+     *    用两个指针，指针 i 从 left 到 right 遍历，指针 j 从 right 到 left 遍历
+     *    当 i 遍历到一个比 mid 大的元素就停下，当 j 遍历到一个比 mid 小的元素就停下，然后交换 arr[i] 和 arr[j]
      */
     public void quickSort(int[] arr, int left, int right) {
         if (left >= right) {
             return;
         }
 
-        int mid = (left + right) / 2;
+        int mid = arr[(left + right) / 2];
         int i = left, j = right;
         while (i < j) {
-            while (arr[i] <= arr[mid] && i < mid) {
+            while (arr[i] < mid) {
                 i++;
             }
-            while (arr[j] >= arr[mid] && j > mid) {
+            while (arr[j] > mid) {
                 j--;
             }
+
+            if (i >= j) {
+                break;
+            }
+
             swap(arr, i, j);
 
-            if (i == mid) {
-                mid = j;
-                i++;
-            } else if (j == mid) {
-                mid = i;
+            if (arr[i] == mid) {
                 j--;
-            } else {
+            }
+            if (arr[j] == mid) {
                 i++;
-                j--;
             }
         }
 
-        quickSort(arr, left, mid - 1);
-        quickSort(arr, mid + 1, right);
+        quickSort(arr, left, j-1);
+        quickSort(arr, i+1, right);
     }
 
     private void swap(int[] arr, int left, int right) {
@@ -57,7 +58,7 @@ public class QuickSort {
 
     @Test
     public void test() {
-        int[] arr = new int[] {5,2,3,1,4,2,4,5,7,3,13,554};
+        int[] arr = new int[] {2,3,1,3,3,43,2,4,5,6,2,1,4,5,56,4,2,12,54,1,4,5532443,21,3,43,4};
         int len = arr.length;
 
         quickSort(arr, 0, len-1);
