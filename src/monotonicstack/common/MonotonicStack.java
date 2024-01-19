40268a52c290be2797cc305ff7d2a9c11daf4f3d
songchongyang.scy <songchongyang.scy@alibaba-inc.com>
2024-01-19 16:18:15 +0800

    单调栈问题

--- src/monotonicstack/common/MonotonicStack.java
+++ src/monotonicstack/common/MonotonicStack.java
@@ -1,5 +1,11 @@
 package monotonicstack.common;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+import org.junit.Test;
+
 /**
  * 单调栈
  *
@@ -16,4 +22,39 @@ package monotonicstack.common;
  *
  */
 public class MonotonicStack {
+
+    /**
+     * 返回 arr 中每个元素右边最近的、比自己大的元素的位置
+     *
+     * -1 表示不存在
+     */
+    public int[] monotonicStack(int[] arr) {
+        int[] answer = new int[arr.length];
+        Arrays.fill(answer, -1);
+
+        // 单调递减栈
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+
+        for (int i = 1; i < arr.length; i++) {
+            // 如果新元素大于栈顶，就弹出栈顶元素，直到新元素满足单调递减的顺序，再把新元素入栈
+            // 对于这些被弹出栈的元素，这个新元素就是在它右边离它最近并且比它大的数
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+                Integer pop = stack.pop();
+                answer[pop] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+
+
+    @Test
+    public void test() {
+        int[] arr = new int[]{13,14,15,11,9,12,16,13};
+        int[] answer = monotonicStack(arr);
+        System.out.println(answer);
+    }
+
 }
