938d07b4537c41b9d4e48955393116f1c4a2837e
scy <1198056107@qq.com>
2024-03-02 13:10:10 +0800

    85. 最大矩形

---
+++ src/monotonicstack/MaxRectangle.java
@@ -0,0 +1,17 @@
+package monotonicstack;
+
+/**
+ * 85. 最大矩形
+ */
+public class MaxRectangle {
+
+    public int maximalRectangle(char[][] matrix) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+
+
+        return 0;
+    }
+
+}
